df39c2235f7e5700111f4af3dea9940a1645680e
powerwave4u <powerwave4u@hanmail.net>
2015-08-11 23:21:59 +0900

    list.py

--- chapter05/list.py
+++ chapter05/list.py
@@ -7,3 +7,24 @@ print(t[1][1])
 s[1] = 100
 print(t)
 
+print()
+L = [1, ['a', ['x', 'y'], 'b'], 3]
+print(L[1])
+print(L[1][1])
+print(L[1][1][1])
+print()
+
+print("********************리스트 메서드***************")
+s = [1, 2, 3]
+s.append(5)
+print(s)
+s.insert(3, 4)
+print(s)
+print(s.index(3))
+print(s.count(2))
+s.reverse()  # 반환값이 없다
+print(s)
+s.sort()  # 반환값이 없다.
+print(s)
+s = [10, 20, 30, 40, 50]
+s.remove(10)
