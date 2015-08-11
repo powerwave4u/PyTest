1d7058c82994760d0403406a1afe927ac3758bf0
powerwave4u <powerwave4u@hanmail.net>
2015-08-12 00:43:55 +0900

    list.py

--- chapter05/list.py
+++ chapter05/list.py
@@ -68,3 +68,73 @@ print(L)
 L.sort(key=str.lower)
 L
 print(L)
+L.sort(key=str.lower, reverse=True)
+print(L)
+print()
+L = ['123', '34', '56', '2345']
+L.sort()
+print(L)
+L.sort(key=int)
+print(L)
+def mykey(a):
+    return a%3
+L = [1, 5, 3, 9, 8, 4, 2]
+L.sort(key=mykey)
+print(L)
+def mykey(a):
+    return (a%3, a)
+L = [1, 5, 3, 9, 8, 4, 2]
+L.sort(key=mykey)
+print(L)
+print()
+print("람다함수")
+L = [1, 5, 3, 9, 8, 4, 2]
+L.sort(key=lambda a : (a%3, a))
+print(L)
+
+print("sorted()함수를 사용한 정렬")
+print()
+L = [('lee', 5, 38), ('kim', 3, 28), ('jung', 10, 36)]
+L.sort(key= lambda a : a[2])
+print(L)
+
+print()
+L = [1, 6, 3, 8, 6, 2, 9]
+newList = L.sort() # 반환값은 없다.
+print(newList)
+
+L = [1, 6, 3, 8, 6, 2, 9]
+newList = sorted(L)
+print(newList)
+
+for ele in sorted(L):
+    print(ele, end='  ')
+
+print()
+L = [1, 6, 3, 8, 6, 2, 9]
+print(sorted(L, reverse=True))
+L = ['123', '34', '56', '2345']
+print(sorted(L, key=int))
+
+d = {'one':1, 'two':2, 'three':3, 'four':4}
+print(sorted(d))
+
+for key in sorted(d):
+    print(key)
+
+print()
+d = {'one':1, 'two':2, 'three':3, 'four':4}
+for key in sorted(d, key=lambda  a : d[a]):
+    print(key)
+
+print()
+print(sorted(d.items(), key=lambda a:a[1]))
+
+print()
+print("revered()함수를 사용한 정렬")
+L = ['2345', '56', '34', '123']
+print(L)
+for ele in reversed(L):
+    print(ele)
+
+
